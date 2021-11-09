12a0fdf4839cbc0b4c602a99c70c998d0c166cfe
takaki <cheap_trick_magic@yahoo.co.jp>
2021-11-09 14:07:33 +0900

    擬似的にレンダリングコストの高いコンポーネントを作成(負荷が高いレンダリング)

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,11 +1,19 @@
 const style = {
-    width: "100%",
-    height: "200px",
-    backgroundColor: "khaki"
+    width: '100%',
+    height: '200px',
+    backgroundColor: 'khaki',
 }
 
 export const ChildArea = (props) => {
     const { open } = props
+    console.log("ChildAreaがレンダリングされた！！")
+
+    const data = [...Array(2000).keys()]
+    console.log(data)
+    data.forEach(() => {
+        console.log("...")
+    })
+
     return (
         <>
             {open ? (
